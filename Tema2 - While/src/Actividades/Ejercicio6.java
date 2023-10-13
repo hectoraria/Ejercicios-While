13d373a31a3875b6efb8beb0962e2576e24f4a6f
hectoraria <145675493+hectoraria@users.noreply.github.com>
2023-10-13 10:53:40 +0200

    ds

---
+++ Tema2 - While/src/Actividades/Ejercicio6.java
@@ -0,0 +1,10 @@
+package Actividades;
+
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Tema2 - While/src/Actividades/Ejercicio7.java
@@ -0,0 +1,10 @@
+package Actividades;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
